64af75132339c3d0edcaa48646aca621ad17533e
Kyle Oba <koba@mudphone.com>
2014-02-24 23:23:55 -1000

    Add bouncing off walls.

--- Bouncing/AAViewController.m
+++ Bouncing/AAViewController.m
@@ -17,30 +17,47 @@
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *ballXConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *ballYConstraint;
 
-@property (assign, nonatomic) CGFloat theta;
+@property (assign, nonatomic) CGPoint velocity;
 @end
 
 @implementation AAViewController
 
-- (void)draw:(CADisplayLink *)sender
+- (void)tick:(CADisplayLink *)sender
 {
-    self.theta += 0.1;
-
+    CGPoint vel = self.velocity;
+    
+    // Bounce of the left and right sides:
     CGFloat width = CGRectGetWidth(self.view.bounds);
-    CGFloat x = width/2.0 * sin(self.theta) + width/2.0;
-    self.ballXConstraint.constant = x;
+    if (CGRectGetMaxX(self.ballView.frame) >= width) {
+        vel.x = -ABS(vel.x);
+    } else if (CGRectGetMinX(self.ballView.frame) <= 0) {
+        vel.x = ABS(vel.x);
+    }
+    
+    // Bounce off the bottom and top:
+    CGFloat height = CGRectGetHeight(self.view.bounds);
+    if (CGRectGetMaxY(self.ballView.frame) >= height) {
+        vel.y = -ABS(vel.y);
+    } else if (CGRectGetMinY(self.ballView.frame) <= 0) {
+        vel.y = ABS(vel.y);
+    }
+    self.velocity = vel;
+    
+    CGPoint pos = CGPointMake(self.ballXConstraint.constant,
+                              self.ballYConstraint.constant);
+    self.ballXConstraint.constant = pos.x + self.velocity.x;
+    self.ballYConstraint.constant = pos.y + self.velocity.y;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
     
-    
-//    CADisplayLink.displayLinkWithTarget( self, @selector(draw:) )
+    self.velocity = CGPointMake(10.0, 10.0);
     
     self.displayLink = [CADisplayLink displayLinkWithTarget:self
-                                                   selector:@selector(draw:)];
+                                                   selector:@selector(tick:)];
     
     [self.displayLink addToRunLoop:[NSRunLoop currentRunLoop]
                            forMode:NSDefaultRunLoopMode];
